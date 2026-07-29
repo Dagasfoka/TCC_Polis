40ee1c3a43696ee7498538e1d838be8509d65aac
Enzo Maximino <enzomaximino2007@gmail.com>
2026-07-28 22:12:18 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173"],
+    allow_origins=["http://127.0.0.1:5173", "https://frontend-ytma.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
