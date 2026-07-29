225a0664d3f281ed547f217035856c8e478b5ae5
Enzo Maximino <enzomaximino2007@gmail.com>
2026-07-28 21:52:53 -0300

    Update redis.py
    Alteração para testar o redis.py via render

--- backend/app/db/redis.py
+++ backend/app/db/redis.py
@@ -1,9 +1,16 @@
 import redis
 from backend.app.core.config import settings
 
-redis_client = redis.Redis(
-    host=settings.REDIS_HOST,
-    port=settings.REDIS_PORT,
-    db=0,
+#redis_client = redis.Redis(
+#    host=settings.REDIS_HOST,
+#   port=settings.REDIS_PORT,
+#    db=0,
+#    decode_responses=True
+#)
+import os
+
+
+redis_client = redis.from_url(
+    os.environ["REDIS_URL"],
     decode_responses=True
 )
