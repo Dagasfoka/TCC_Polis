a612c26e45a58f7d9b6c3fb03ad07658e87c69e3
Enzo Maximino <enzomaximino2007@gmail.com>
2026-08-06 21:51:35 -0300

    Update match.py

--- backend/app/api/routes/redis/match.py
+++ backend/app/api/routes/redis/match.py
@@ -1,11 +1,11 @@
 # Rotas HTTP auxiliares da partida.
-from backend.app.core.config import Settings
+from backend.app.core.config import settings
 from fastapi import APIRouter
 from fastapi.templating import Jinja2Templates
 
 from backend.app.services.redis.match_service import get_match
 from backend.app.scripts.create_demo_match import create_demo_match
-from backend.app.scripts.create_db import create_db
+from backend.app.scripts.create_db import create_database
 from backend.app.scripts.seed_parties import seed_parties
 from backend.app.scripts.seed_missions import seed_missions
 from backend.app.scripts.seed_territories import seed_territories
@@ -28,5 +28,5 @@ async def create_demo_match_route():
 @router_match.get("/debug/db")
 async def debug_db():
     return {
-        "database_url": Settings.DATABASE_URL
+        "database_url": settings.DATABASE_URL
     }
