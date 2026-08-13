8919b90e90f4555d4b9f3c8f66d68aeab52876b2
Enzo Maximino <enzomaximino2007@gmail.com>
2026-08-13 19:04:27 -0300

    Create question.py

---
+++ backend/app/models/db/question.py
@@ -0,0 +1,30 @@
+# backend/app/models/db/question.py
+
+from sqlalchemy import Boolean, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backend.app.db.base import Base
+
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    question_id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True
+    )
+
+    subject: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False
+    )
+
+    description: Mapped[str] = mapped_column(
+        String(500),
+        nullable=False
+    )
+
+    answer: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable=False
+    )
