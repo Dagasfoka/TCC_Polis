f679c90b2758dd2a4a8b0f35986d33442ddfe34b
Enzo Maximino <enzomaximino2007@gmail.com>
2026-08-13 19:44:13 -0300

    Update questions_gateways.py

--- backend/app/gateways/questions_gateways.py
+++ backend/app/gateways/questions_gateways.py
@@ -1,6 +1,6 @@
 from backend.app.repositories.db.question_repo import  QuestionRepo
 class QuestionGateways:
-    def __init__(self) -> None:
-        self.question_repo=QuestionRepo()
+    def __init__(self, db: Session) -> None:
+        self.question_repo=QuestionRepo(db)
     def get_all_questions(self):
         return self.question_repo.get_all_questions()
