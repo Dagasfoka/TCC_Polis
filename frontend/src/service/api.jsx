68581c8a8fae9f0641cabfd6b847cab5ee113b10
koga43656 <129287351+koga43656@users.noreply.github.com>
2026-09-25 14:49:03 -0300

    Add files via upload

--- frontend/src/service/api.jsx
+++ frontend/src/service/api.jsx
@@ -1,6 +1,7 @@
 
 const API_URL = (
-  import.meta.env.VITE_API_URL || "http://localhost:8000"
+  import.meta.env.VITE_API_URL ||
+  "https://tcc-polis-42o9.onrender.com"
 ).replace(/\/$/, "");
 
 // Função central para fazer requisições ao FastAPI.
